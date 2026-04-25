5fc3acca47fa753e845760105469495f7477dc18
zhoucheng <zhoucheng133@outlook.com>
2026-04-25 21:22:40 +0800

    添加标题栏

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="titlebar" data-tauri-drag-region></div>
+</template>
+
+<style scoped>
+.titlebar{
+  background-color: transparent;
+  width: 100%;
+  height: 30px;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+</style>
