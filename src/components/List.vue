52412251e4cdc1f776a0f8e500324044a4e19247
zhoucheng <zhoucheng133@outlook.com>
2026-04-26 19:21:05 +0800

    完成搜索功能

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,46 @@
 <template>
-  <div class="list_header">
-
+  <div class="list_container" v-if="list.length!=0">
+    <div class="list_header">
+      <div class="list_header_content">
+        <div class="list_header_center">序号</div>
+        <div>标题</div>
+        <div>艺人</div>
+        <div class="list_header_center"><v-icon icon="mdi-timer-outline"></v-icon></div>
+        <div class="list_header_center"><v-icon icon="mdi-link"></v-icon></div>
+      </div>
+      <div class="list_divider"></div>
+      <div class="list_result_content" v-for="(item, index) in list" :key="index">
+        <div class="list_header_center">{{ index+1 }}</div>
+        <div>{{ item.name }}</div>
+        <div>{{ item.artist }}</div>
+        <div class="list_header_center">{{ item.duration }}</div>
+        <div class="list_header_center">
+          <v-btn icon="mdi-download" variant="text" size="small"></v-btn>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="list_empty" v-else-if="!loading">
+    <div class="list_empty_content">
+      <v-icon icon="mdi-music-circle-outline"></v-icon>
+      <div style="margin-top: 5px; font-size: 14px;">搜索关键词来查找</div>
+    </div>
+  </div>
+  <div class="list_empty" v-else-if="loading">
+    <v-progress-circular
+      indeterminate
+      :size="30"
+      :width="4"
+      color="rgb(20, 118, 108)"
+    ></v-progress-circular>
   </div>
 </template>
 
 <script lang="ts" setup>
+import "../styles/list.css";
+import Store from "../store/index";
+import { storeToRefs } from "pinia";
+const store = Store();
+let {list, loading} = storeToRefs(store);
+
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,77 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { message } from '@tauri-apps/plugin-dialog';
+import { Command } from '@tauri-apps/plugin-shell';
 
 export enum Page{
   Home,
   Settings,
 }
 
+export class ListItem{
+  constructor(
+    public name: string, 
+    public artist: string,
+    public album: string,
+    public url: string,
+    public cover: string,
+    public duration: string,
+  ){}
+}
+
 export default defineStore("index", ()=>{
   const page=ref(Page.Home);
   const client=ref('NeteaseMusicClient');
   const keyword=ref('');
+  const loading=ref(false);
   const search=async ()=>{
     if(keyword.value.length === 0){
-      message('搜索关键词不能为空', { title: '无法搜索', kind: 'error' });
+      await message('搜索关键词不能为空', { title: '无法搜索', kind: 'error' });
+      return;
     }
+    loading.value=true;
+    const args=[
+      "-keyword", keyword.value,
+      "-client", client.value,
+      // 临时
+      "-workdir", "/Users/zhoucheng/musicdl",
+    ]
+
+    const command = Command.sidecar('binaries/core', args, {
+      encoding: "utf-8",
+      env: {
+        "PYTHONUTF8": "1",
+        "LANG": "en_US.UTF-8",
+        "LC_ALL": "en_US.UTF-8"
+      }
+    });
+    const output = await command.execute();
+    const match = output.stdout.match(/===JSON_START===\n([\s\S]*)\n===JSON_END===/);
+    if(match){
+      const jsonData = JSON.parse(match[1]);
+      console.log(jsonData);
+      list.value=jsonData.map((item: any)=>{
+        const formattedTime = item.duration.replace(/^00:/, '');
+        return new ListItem(
+          item.name,
+          item.artist,
+          item.album,
+          item.url,
+          item.cover,
+          formattedTime,
+        );
+      });
+    }
+    loading.value=false;
   };
+  const list=ref<ListItem[]>([]);
 
   return {
     page,
     client,
     keyword,
-    search
+    search,
+    list,
+    loading
   };
 });
